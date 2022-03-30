58437d886ea3cafabd3ca8e624a31b8d0c86f100
Cesar David Botina <cesarbotina98@gmail.com>
2022-03-30 15:54:52 -0500

    se agrega funcionalidad al sidebar y rutas

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,13 +10,15 @@ import { PagesComponent } from './pages.component';
 
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from './../components/components.module';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ],
   imports: [
     CommonModule,
@@ -29,7 +31,8 @@ import { ComponentsModule } from './../components/components.module';
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ]
 })
 export class PagesModule { }
